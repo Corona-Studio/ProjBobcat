bb798543abb7f84024767da68520f9f4f9d35076
Eric Stevens <fjnhhty777@hotmail.com>
2020-02-21 21:13:01 +0800

    Fix commit conflict.

--- DefaultComponent/Launch/DefaultGameCore.cs
+++ DefaultComponent/Launch/DefaultGameCore.cs
@@ -19,13 +19,14 @@ namespace ProjBobcat.DefaultComponent.Launch
 {
     public class DefaultGameCore : IGameCore, IDisposable
     {
-        public string RootPath { get; set; }
+        private string AbsRootPath;
+        public string RootPath { get => AbsRootPath; set => AbsRootPath = Path.GetFullPath(value?.TrimEnd('/') + "//") + "//"; }
         public IVersionLocator VersionLocator { get; set; }
         public Guid ClientToken { get; set; }
 
         public event EventHandler<GameExitEventArgs> GameExitEventDelegate;
         public event EventHandler<GameLogEventArgs> GameLogEventDelegate;
-        public event EventHandler<LaunchLogEventArgs> LaunchLogEventDelegate; 
+        public event EventHandler<LaunchLogEventArgs> LaunchLogEventDelegate;
 
         /// <summary>
         /// 启动游戏。
@@ -129,28 +130,31 @@ namespace ProjBobcat.DefaultComponent.Launch
                     if (!Directory.Exists(argumentParser.NativeRoot))
                         Directory.CreateDirectory(argumentParser.NativeRoot);
 
-                    //TODO
-                    DirectoryHelper.CleanDirectory(argumentParser.NativeRoot, false);
-                    version.Natives.ForEach(n =>
+                    if (!File.Exists(argumentParser.NativeRoot + "\\.cmfl"))
                     {
-                        var path =
-                            $"{GamePathHelper.GetLibraryPath(RootPath.TrimEnd('\\'), string.Empty)}\\{n.FileInfo.Path.Replace('/', '\\')}";
-                        using var stream =
-                            File.OpenRead(path);
-                        using var reader = ReaderFactory.Open(stream);
-                        while (reader.MoveToNextEntry())
+                        DirectoryHelper.CleanDirectory(argumentParser.NativeRoot, false);
+                        version.Natives.ForEach(n =>
                         {
-                            if (!(n.Extract?.Exclude?.Contains(reader.Entry.Key) ?? false))
+                            var path =
+                                $"{GamePathHelper.GetLibraryPath(RootPath.TrimEnd('\\'), string.Empty)}\\{n.FileInfo.Path.Replace('/', '\\')}";
+                            using var stream =
+                                File.OpenRead(path);
+                            using var reader = ReaderFactory.Open(stream);
+                            while (reader.MoveToNextEntry())
                             {
-                                reader.WriteEntryToDirectory(argumentParser.NativeRoot,
-                                    new ExtractionOptions
-                                    {
-                                        ExtractFullPath = true,
-                                        Overwrite = true
-                                    });
+                                if (!(n.Extract?.Exclude?.Contains(reader.Entry.Key) ?? false))
+                                {
+                                    reader.WriteEntryToDirectory(argumentParser.NativeRoot,
+                                        new ExtractionOptions
+                                        {
+                                            ExtractFullPath = true,
+                                            Overwrite = true
+                                        });
+                                }
                             }
-                        }
-                    });
+                        });
+                        File.Create(argumentParser.NativeRoot + "\\.cmfl");
+                    }
                 }
                 catch (Exception e)
                 {
