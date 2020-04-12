373899a8ffcd3911c17dbfec84b0d1e777a9a8e2
yueyinqiu <lujiaqi2003@126.com>
2020-04-12 22:19:37 +0800

    IAuthenticator.cs RawVersionModel.cs

--- Interface/IAuthenticator.cs
+++ Interface/IAuthenticator.cs
@@ -3,6 +3,9 @@ using ProjBobcat.Class.Model;
 
 namespace ProjBobcat.Interface
 {
+    /// <summary>
+    /// 表示一个验证器。
+    /// </summary>
     public interface IAuthenticator
     {
         ILauncherProfileParser LauncherProfileParser { get; set; }
