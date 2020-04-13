59668288750e4a6c20921fa2dc242ea7b5021215
yueyinqiu <lujiaqi2003@126.com>
2020-04-13 13:17:28 +0800

    优化DirectoryHelper

--- Class/Helper/DirectoryHelper.cs
+++ Class/Helper/DirectoryHelper.cs
@@ -4,12 +4,16 @@ namespace ProjBobcat.Class.Helper
 {
     public static class DirectoryHelper
     {
-        public static void CleanDirectory(string path, bool deleteDirectory)
+        public static void CleanDirectory(string path, bool deleteDirectory = false)
         {
             var directory = new DirectoryInfo(path);
-            foreach (var file in directory.GetFiles()) file.Delete();
-            foreach (var subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
-            if (deleteDirectory) directory.Delete();
+            if (deleteDirectory)
+                directory.Delete(true);
+            else
+            {
+                foreach (var file in directory.GetFiles()) file.Delete();
+                foreach (var subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+            }
         }
     }
 }
