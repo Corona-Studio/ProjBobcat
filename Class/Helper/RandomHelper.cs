481e5df16bf152b1272426551f404f140eced3a2
yueyinqiu <lujiaqi2003@126.com>
2020-04-13 22:30:38 +0800

    RandomHelper注释

--- Class/Helper/RandomHelper.cs
+++ Class/Helper/RandomHelper.cs
@@ -4,10 +4,23 @@ using System.Linq;
 
 namespace ProjBobcat.Class.Helper
 {
+    /// <summary>
+    /// 随机数帮助器。
+    /// </summary>
     public static class RandomHelper
     {
         private static readonly Random Random = new Random();
-
+        /// <summary>
+        /// 在一个有限的 <see cref="IEnumerable{T}"/> 中等概率地随机取出一项返回。
+        /// </summary>
+        /// <typeparam name="T">数据的类型。</typeparam>
+        /// <param name="enumerable">
+        /// 一个有限的 <see cref="IEnumerable{T}"/> ：
+        /// 若其中元素数为 0 ，则会返回 <typeparamref name="T"/> 对应的默认值；
+        /// 若其中有无限的元素，程序可能会长时间在此停留，直至出现异常，这个异常是不可预测的，经过测试及相关分析已知有可能是 <see cref="IndexOutOfRangeException"/> 和 <see cref="OutOfMemoryException"/> 。
+        /// </param>
+        /// <returns> <paramref name="enumerable"/> 中的随机一项。</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static T RandomSample<T>(this IEnumerable<T> enumerable)
         {
             var arr = enumerable.ToArray();
