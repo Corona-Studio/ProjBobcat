16973507d94f4bd318b3e4d52b62a73e02f543cf
yueyinqiu <lujiaqi2003@126.com>
2020-04-14 09:54:13 +0800

    优化RandomStringHelper，并添加若干方法。

--- Class/Helper/RandomStringHelper.cs
+++ Class/Helper/RandomStringHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -30,7 +31,7 @@ namespace ProjBobcat.Class.Helper
         private readonly List<char> enabled;
         /// <summary>
         /// 获取该帮助器已启用的字符所构成的字符串。
-        /// 如果需要移除部分字符，请创建一个新的实例。
+        /// 如果需要移除部分字符，请创建一个新的实例并重新添加。
         /// </summary>
         public string EnabledCharacters => new string(enabled.ToArray());
 
@@ -44,51 +45,82 @@ namespace ProjBobcat.Class.Helper
         /// <summary>
         /// 加入数字字符。
         /// 这个方法将使得 <seealso cref="Numbers"/> 中的字符被加入。
-        /// 允许多次调用，如果出现多次调用，将提升在生成此类字符在结果中出现的概率。
+        /// 允许多次调用，如果出现多次调用，将提升此类字符的权重。
         /// </summary>
         /// <returns>帮助器本身。</returns>
         public RandomStringHelper UseNumbers()
         {
-            enabled.AddRange(Numbers.ToCharArray().ToList());
+            enabled.AddRange(Numbers);
             return this;
         }
 
         /// <summary>
         /// 加入小写字母字符。
         /// 这个方法将使得 <seealso cref="LowerCases"/> 中的字符被加入。
-        /// 允许多次调用，如果出现多次调用，将提升在生成此类字符在结果中出现的概率。
+        /// 允许多次调用，如果出现多次调用，将提升此类字符的权重。
         /// </summary>
         /// <returns>帮助器本身。</returns>
         public RandomStringHelper UseLower()
         {
-            enabled.AddRange(LowerCases.ToCharArray().ToList());
+            enabled.AddRange(LowerCases);
             return this;
         }
 
         /// <summary>
         /// 加入大写字母字符。
         /// 这个方法将使得 <seealso cref="UpperCases"/> 中的字符被加入。
-        /// 允许多次调用，如果出现多次调用，将提升在生成此类字符在结果中出现的概率。
+        /// 允许多次调用，如果出现多次调用，将提升此类字符的权重。
         /// </summary>
         /// <returns>帮助器本身。</returns>
         public RandomStringHelper UseUpper()
         {
-            enabled.AddRange(UpperCases.ToCharArray().ToList());
+            enabled.AddRange(UpperCases);
             return this;
         }
 
         /// <summary>
         /// 加入标点字符。
         /// 这个方法将使得 <seealso cref="Symbols"/> 中的字符被加入。
-        /// 允许多次调用，如果出现多次调用，将提升在生成此类字符在结果中出现的概率。
+        /// 允许多次调用，如果出现多次调用，将提升此类字符的权重。
         /// </summary>
         /// <returns>帮助器本身。</returns>
         public RandomStringHelper UseSymbols()
         {
-            enabled.AddRange(Symbols.ToCharArray().ToList());
+            enabled.AddRange(Symbols);
             return this;
         }
 
+
+        /// <summary>
+        /// 加入一些字符。
+        /// 这个方法将使得 <paramref name="characters"/> 中的元素被加入。
+        /// 允许多次加入同一字符，这将提升此字符的权重。
+        /// </summary>
+        /// <param name="characters">要加入的字符。</param>
+        /// <returns>帮助器本身。</returns>
+        public RandomStringHelper UseCharacters(params char[] characters)
+        {
+            enabled.AddRange(characters);
+            return this;
+        }
+        /// <summary>
+        /// 加入一些字符。
+        /// 这个方法将使得 <paramref name="characters"/> 中的元素被加入。
+        /// 允许多次加入同一字符，这将提升此字符的权重。
+        /// </summary>
+        /// <param name="characters">要加入的字符。</param>
+        /// <returns>帮助器本身。</returns>
+        public RandomStringHelper UseCharacters(IEnumerable<char> characters)
+        {
+            enabled.AddRange(characters);
+            return this;
+        }
+        /// <summary>
+        /// 打乱顺序。此方法是不需要的。
+        /// </summary>
+        /// <param name="times">打乱次数。</param>
+        /// <returns>帮助器本身。</returns>
+        [Obsolete("不需要调用此方法。")]
         public RandomStringHelper HardMix(int times)
         {
             var range = Enumerable.Range(0, enabled.Count - 1).ToArray();
