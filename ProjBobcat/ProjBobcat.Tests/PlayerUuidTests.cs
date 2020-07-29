5eb16e1c28769a26370fe9faa1864564267267c1
laolarou <luolinrui726@outlook.com>
2020-07-29 20:46:09 +0800

    Minor Bug Fix

---
+++ ProjBobcat/ProjBobcat.Tests/PlayerUuidTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using ProjBobcat.Class.Model;
+
+namespace ProjBobcat.Tests
+{
+    [TestClass]
+    public class PlayerUuidTests
+    {
+        [TestMethod]
+        public void JsonSTest()
+        {
+            var strs = new Dictionary<PlayerUUID, string> {{PlayerUUID.Random(), "abc"}};
+            var result = JsonConvert.SerializeObject(strs);
+            var r2 = JsonConvert.DeserializeObject<Dictionary<PlayerUUID, string>>(result);
+            var result2 = JsonConvert.SerializeObject(r2);
+
+            Assert.IsTrue(result == result2);
+        }
+    }
+}
