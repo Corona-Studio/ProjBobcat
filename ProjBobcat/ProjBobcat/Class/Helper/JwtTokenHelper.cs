e43cbf26b52d4cfb4b839586ef380c0146f44ccf
laolarou <luolinrui726@outlook.com>
2022-05-22 18:44:06 -0700

    bug fix

--- ProjBobcat/ProjBobcat/Class/Helper/JwtTokenHelper.cs
+++ ProjBobcat/ProjBobcat/Class/Helper/JwtTokenHelper.cs
@@ -1,21 +1,37 @@
-﻿using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
 
 namespace ProjBobcat.Class.Helper;
 
 public static class JwtTokenHelper
 {
+    static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var payload = jwt.Split('.')[1];
+        var jsonBytes = Convert.FromBase64String(ReformatBase64String(payload));
+        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+        return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+    }
+
+    static string ReformatBase64String(string str)
+    {
+        str = str.Replace('_', '/').Replace('-', '+');
+        switch (str.Length % 4)
+        {
+            case 2: str += "=="; break;
+            case 3: str += "="; break;
+        }
+
+        return str;
+    }
+
     public static Dictionary<string, string> GetTokenInfo(string token)
     {
-        var result = new Dictionary<string, string>();
+        var claims = ParseClaimsFromJwt(token);
 
-        var handler = new JwtSecurityTokenHandler();
-        var jwtSecurityToken = handler.ReadJwtToken(token);
-        var claims = jwtSecurityToken.Claims.ToList();
-
-        foreach (var claim in claims) result.Add(claim.Type, claim.Value);
-
-        return result;
+        return claims.ToDictionary(claim => claim.Type, claim => claim.Value);
     }
 }
