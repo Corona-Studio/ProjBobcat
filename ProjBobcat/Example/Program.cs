086864c75fd5bf745219e6310b85a37d22e447b9
laolarou <luolinrui726@outlook.com>
2023-01-20 23:41:16 -0800

    add more example

--- ProjBobcat/Example/Program.cs
+++ ProjBobcat/Example/Program.cs
@@ -13,48 +13,58 @@ using System.Collections.Generic;
 
 namespace Example
 {
-    internal class Program
+    class Program
     {
-        public static DefaultGameCore core;
+        static DefaultGameCore _core;
 
-        private static async Task Main(string[] args)
+        static async Task Main()
         {
             var jl = SystemInfoHelper.FindJava();
             var javaResult = new List<string>();
+            var jIndex = 0;
 
             await foreach (var java in jl)
             {
-                Console.WriteLine($"搜索到的 Java - {java}");
+                Console.WriteLine($"[{jIndex + 1}] 搜索到的 Java - {java}");
                 javaResult.Add(java);
+                jIndex++;
             }
+
+            Console.Write($"选择将要用于启动游戏的 Java 运行时 （1 - {jIndex}）：");
+            var selectedJavaIndex = Convert.ToInt32(Console.ReadLine()) - 1;
 
             InitLauncherCore(); //初始化核心
 
-            Console.WriteLine($"在目录中扫描到了 {core.VersionLocator.GetAllGames().Count()} 个游戏：");
-            var gameList = core.VersionLocator.GetAllGames().ToList();
+            Console.WriteLine($"在目录中扫描到了 {_core.VersionLocator.GetAllGames().Count()} 个游戏：");
+            var gameList = _core.VersionLocator.GetAllGames().ToList();
+            var gameIndex = 0;
+
             foreach (var game in gameList)
             {
-                Console.WriteLine($"\t - {game.Name}");
+                Console.WriteLine($"\t [{gameIndex + 1}] - {game.Name}");
+                gameIndex++;
             }
 
-            var firstGame = gameList.First(); //获取搜索到的第一个游戏
-            Console.WriteLine(
-                string.Format("第一个游戏的信息：{0}游戏 ID：{1}{0}游戏底层版本：{2}{0}游戏名称：{3}", 
-                $"{Environment.NewLine}\t",
-                firstGame.Id, 
-                firstGame.RootVersion, 
-                firstGame.Name));
+            Console.WriteLine($"选择将要用于启动的游戏 （1 - {gameIndex}）：");
+            var selectedGameIndex = Convert.ToInt32(Console.ReadLine()) - 1;
 
-            var javaPath = javaResult.First(); //获取搜索到的第一个Java
-            Console.WriteLine($"列表中的第一个 Java 所在的路径：{javaPath}");
+            var selectedGame = gameList[selectedGameIndex]; //获取搜索到的第一个游戏
+            Console.WriteLine("第一个游戏的信息：{0}游戏 ID：{1}{0}游戏底层版本：{2}{0}游戏名称：{3}",
+                $"{Environment.NewLine}\t",
+                selectedGame.Id,
+                selectedGame.RootVersion,
+                selectedGame.Name);
+
+            var javaPath = javaResult[selectedJavaIndex]; //获取搜索到的第一个Java
+            Console.WriteLine($"选择的 Java 运行时路径：{javaPath}");
 
             var launchSettings = new LaunchSettings
             {
-                Version = firstGame.Id, // 需要启动的游戏ID
+                Version = selectedGame.Id, // 需要启动的游戏ID
                 VersionInsulation = false, // 版本隔离
-                GameResourcePath = core.RootPath, // 资源根目录
-                GamePath = core.RootPath, // 游戏根目录，如果有版本隔离则应该改为GamePathHelper.GetGamePath(Core.RootPath, versionId)
-                VersionLocator = core.VersionLocator, // 游戏定位器
+                GameResourcePath = _core.RootPath, // 资源根目录
+                GamePath = _core.RootPath, // 游戏根目录，如果有版本隔离则应该改为GamePathHelper.GetGamePath(Core.RootPath, versionId)
+                VersionLocator = _core.VersionLocator, // 游戏定位器
                 GameName = gameList[0].Name,
                 GameArguments = new GameArguments // （可选）具体游戏启动参数
                 {
@@ -68,40 +78,40 @@ namespace Example
                 Authenticator = new OfflineAuthenticator //离线认证
                 {
                     Username = "test", //离线用户名
-                    LauncherAccountParser = core.VersionLocator.LauncherAccountParser
+                    LauncherAccountParser = _core.VersionLocator.LauncherAccountParser
                 }
             };
 
 
-            core.GameLogEventDelegate += Core_GameLogEventDelegate;
-            core.LaunchLogEventDelegate += Core_LaunchLogEventDelegate;
-            core.GameExitEventDelegate += Core_GameExitEventDelegate;
-            var result = await core.LaunchTaskAsync(launchSettings);
+            _core.GameLogEventDelegate += Core_GameLogEventDelegate;
+            _core.LaunchLogEventDelegate += Core_LaunchLogEventDelegate;
+            _core.GameExitEventDelegate += Core_GameExitEventDelegate;
+            var result = await _core.LaunchTaskAsync(launchSettings);
             Console.WriteLine(result.Error?.Exception);
             Console.ReadLine();
         }
 
-        private static void Core_GameExitEventDelegate(object sender, GameExitEventArgs e)
+        static void Core_GameExitEventDelegate(object sender, GameExitEventArgs e)
         {
             Console.WriteLine("DONE");
         }
 
-        private static void Core_LaunchLogEventDelegate(object sender, LaunchLogEventArgs e)
+        static void Core_LaunchLogEventDelegate(object sender, LaunchLogEventArgs e)
         {
             Console.WriteLine($"[启动 LOG] - {e.Item}");
         }
 
-        private static void Core_GameLogEventDelegate(object sender, GameLogEventArgs e)
+        static void Core_GameLogEventDelegate(object sender, GameLogEventArgs e)
         {
             Console.WriteLine($"[游戏 LOG] - {e.Content}");
         }
 
-        public static void InitLauncherCore()
+        static void InitLauncherCore()
         {
             var clientToken = new Guid("88888888-8888-8888-8888-888888888888");
             //var rootPath = Path.GetFullPath(".minecraft\");
-            var rootPath = ".minecraft";
-            core = new DefaultGameCore
+            const string rootPath = ".minecraft";
+            _core = new DefaultGameCore
             {
                 ClientToken = clientToken,
                 RootPath = rootPath,
