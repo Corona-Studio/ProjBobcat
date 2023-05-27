f8d10aeb31b617ee95cf44df2c247601d9474f16
Jack Lei <58357771+imJack6@users.noreply.github.com>
2023-05-27 15:20:33 +0800

    Add Get_AppxPackage

--- ProjBobcat/ProjBobcat/Platforms/Windows/SystemInfoHelper.Windows.cs
+++ ProjBobcat/ProjBobcat/Platforms/Windows/SystemInfoHelper.Windows.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,7 +9,7 @@ using ProjBobcat.Class.Model;
 
 namespace ProjBobcat.Platforms.Windows;
 
-static class SystemInfoHelper
+public static class SystemInfoHelper
 {
     static readonly PerformanceCounter FreeMemCounter = new("Memory", "Available MBytes");
     static readonly PerformanceCounter MemUsagePercentageCounter = new("Memory", "% Committed Bytes In Use");
@@ -27,7 +27,15 @@ static class SystemInfoHelper
     ///     判断是否安装了 UWP 版本的 Minecraft 。
     /// </summary>
     /// <returns>判断结果。</returns>
-    public static bool IsMinecraftUWPInstalled()
+    public static bool IsMinecraftUWPInstalled() =>
+        !string.IsNullOrEmpty(Get_AppxPackage("Microsoft.MinecraftUWP").Status);
+
+        /// <summary>
+    ///     获取 UWP 应用的信息。
+    /// </summary>
+    /// <param name="appName">应用名称</param>
+    /// <returns>GetAppxPackage</returns>
+    public static GetAppxPackage Get_AppxPackage(string appName)
     {
         using var process = new Process
         {
@@ -36,16 +44,104 @@ static class SystemInfoHelper
                 WorkingDirectory = Environment.CurrentDirectory,
                 RedirectStandardOutput = true,
                 CreateNoWindow = true,
-                Arguments = "Get-AppxPackage -Name \"Microsoft.MinecraftUWP\""
+                Arguments = $"Get-AppxPackage -Name \"{appName}\""
             }
         };
-
         process.Start();
-
         var reader = process.StandardOutput;
-        return !string.IsNullOrEmpty(reader.ReadToEnd());
-    }
 
+        Dictionary<string, string> values = new Dictionary<string, string>();
+        string[] lines = reader.ReadToEnd().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+        string key = null;
+        string value = null;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            if (line.Contains(":"))
+            {
+                if (key != null)
+                {
+                    values[key] = value.TrimEnd();
+                }
+                string[] parts = line.Split(new[] { ':' }, 2, StringSplitOptions.None);
+                key = parts[0].Trim();
+                value = parts[1].Trim();
+            }
+            else if (key != null)
+            {
+                value += line.Trim();
+            }
+        }
+        if (key != null)
+        {
+            values[key] = value.TrimEnd();
+        }
+
+        GetAppxPackage appxPackage = new GetAppxPackage();
+        foreach (KeyValuePair<string, string> pair in values)
+        {
+            switch (pair.Key)
+            {
+                case "Name":
+                    appxPackage.Name = pair.Value;
+                    break;
+                case "Publisher":
+                    appxPackage.Publisher = pair.Value;
+                    break;
+                case "Architecture":
+                    appxPackage.Architecture = pair.Value;
+                    break;
+                case "ResourceId":
+                    appxPackage.ResourceId = pair.Value;
+                    break;
+                case "Version":
+                    appxPackage.Version = pair.Value;
+                    break;
+                case "PackageFullName":
+                    appxPackage.PackageFullName = pair.Value;
+                    break;
+                case "InstallLocation":
+                    appxPackage.InstallLocation = pair.Value;
+                    break;
+                case "IsFramework":
+                    appxPackage.IsFramework = Convert.ToBoolean(pair.Value);
+                    break;
+                case "PackageFamilyName":
+                    appxPackage.PackageFamilyName = pair.Value;
+                    break;
+                case "PublisherId":
+                    appxPackage.PublisherId = pair.Value;
+                    break;
+                case "IsResourcePackage":
+                    appxPackage.IsResourcePackage = Convert.ToBoolean(pair.Value);
+                    break;
+                case "IsBundle":
+                    appxPackage.IsBundle = Convert.ToBoolean(pair.Value);
+                    break;
+                case "IsDevelopmentMode":
+                    appxPackage.IsDevelopmentMode = Convert.ToBoolean(pair.Value);
+                    break;
+                case "NonRemovable":
+                    appxPackage.NonRemovable = Convert.ToBoolean(pair.Value);
+                    break;
+                case "Dependencies":
+                    appxPackage.Dependencies = pair.Value.TrimStart('{').TrimEnd('}').Split(',').Select(s => s.Trim()).ToArray();
+                    break;
+                case "IsPartiallyStaged":
+                    appxPackage.IsPartiallyStaged = Convert.ToBoolean(pair.Value);
+                    break;
+                case "SignatureKind":
+                    appxPackage.SignatureKind = pair.Value;
+                    break;
+                case "Status":
+                    appxPackage.Status = pair.Value;
+                    break;
+            }
+        }
+        return appxPackage;
+    }
+    
     /// <summary>
     ///     从注册表中查找可能的 javaw.exe 的路径。
     /// </summary>
