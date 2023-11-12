4acf1293d13084d81833512e0d3eff2434ca9db5
laolarou <luolinrui726@outlook.com>
2023-11-11 23:25:56 -0800

    Update GameLogEventArgs.cs

--- ProjBobcat/ProjBobcat/Event/GameLogEventArgs.cs
+++ ProjBobcat/ProjBobcat/Event/GameLogEventArgs.cs
@@ -6,11 +6,11 @@ namespace ProjBobcat.Event;
 public class GameLogEventArgs : EventArgs
 {
     public GameLogType LogType { get; set; }
-    public string Time { get; set; }
-    public string Source { get; set; }
-    public string RawContent { get; set; }
-    public string Content { get; set; }
+    public string? Time { get; set; }
+    public string? Source { get; set; }
+    public string? RawContent { get; set; }
+    public string? Content { get; set; }
 
-    public string ExceptionMsg { get; set; }
-    public string StackTrace { get; set; }
+    public string? ExceptionMsg { get; set; }
+    public string? StackTrace { get; set; }
 }
