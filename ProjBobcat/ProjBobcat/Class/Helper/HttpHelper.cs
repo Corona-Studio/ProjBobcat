26805ab9e22e4e79a530e733ff449292a5f1c9f0
laolarou <luolinrui726@outlook.com>
2023-11-18 20:16:52 -0800

    Update HttpHelper.cs

--- ProjBobcat/ProjBobcat/Class/Helper/HttpHelper.cs
+++ ProjBobcat/ProjBobcat/Class/Helper/HttpHelper.cs
@@ -18,7 +18,7 @@ public static partial class HttpHelper
         "((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+$,\\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:ww‌​w.|[-;:&=\\+$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?‌​(?:[\\w]*))?)";
 
 #if NET8_0_OR_GREATER
-    [GeneratedRegex("((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+$,\\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:ww‌​w.|[-;:&=\\+$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?‌​(?:[\\w]*))?)")]
+    [GeneratedRegex(UriRegexStr)]
     private static partial Regex UriRegex();
 
 #else
