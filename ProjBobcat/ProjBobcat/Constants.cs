8d783b011c1890cdaf28d2c71ca10120aca7e71a
BiDuang <me@biduang.cn>
2023-11-30 10:53:28 +0800

    fix: Full support for runtime cross platform

--- ProjBobcat/ProjBobcat/Constants.cs
+++ ProjBobcat/ProjBobcat/Constants.cs
@@ -1,26 +1,56 @@
-﻿namespace ProjBobcat;
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ProjBobcat;
 
 public static class Constants
 {
     public const string FallBackVersion = "0.0.0";
 
-#if WINDOWS
-    public const string WhereCommand = "where";
-    public const string JavaExecutable = "javaw.exe";
-    public const string JavaExecutablePath = $"bin\\{JavaExecutable}";
-    public const string JavaExecutableExtension = "exe";
-    public const string OsSymbol = "windows";
-#elif OSX
-    public const string WhereCommand = "whereis";
-    public const string JavaExecutable = "java";
-    public const string JavaExecutablePath = $"Contents/Home/bin/{JavaExecutable}";
-    public const string JavaExecutableExtension = "*";
-    public const string OsSymbol = "osx";
-#elif LINUX
-    public const string WhereCommand = "whereis";
-    public const string JavaExecutable = "java";
-    public const string JavaExecutablePath = $"bin/{JavaExecutable}";
-    public const string JavaExecutableExtension = "*";
-    public const string OsSymbol = "linux";
-#endif
+    public static string WhereCommand => OperatingSystem.IsWindows() ? Windows.WhereCommand : Linux.WhereCommand;
+    public static string JavaExecutable => OperatingSystem.IsWindows() ? Windows.JavaExecutable : Linux.JavaExecutable;
+
+    public static string JavaExecutableExtension => OperatingSystem.IsWindows()
+        ? Windows.JavaExecutableExtension
+        : Linux.JavaExecutableExtension;
+
+    public static string JavaExecutablePath => RuntimeInformation.RuntimeIdentifier switch
+    {
+        "windows" => Windows.JavaExecutablePath,
+        "linux" => Linux.JavaExecutablePath,
+        "osx" => Osx.JavaExecutablePath,
+        _ => throw new PlatformNotSupportedException("Unknown operating system.")
+    };
+
+    public static string OsSymbol => RuntimeInformation.RuntimeIdentifier switch
+    {
+        "windows" => Windows.OsSymbol,
+        "linux" => Linux.OsSymbol,
+        "osx" => Osx.OsSymbol,
+        _ => throw new PlatformNotSupportedException("Unknown operating system.")
+    };
+
+    class Windows
+    {
+        public const string WhereCommand = "where";
+        public const string JavaExecutable = "javaw.exe";
+        public const string JavaExecutablePath = $"bin\\{JavaExecutable}";
+        public const string JavaExecutableExtension = "exe";
+        public const string OsSymbol = "windows";
+    }
+
+    class Linux
+    {
+        public const string WhereCommand = "whereis";
+        public const string JavaExecutable = "java";
+        public const string JavaExecutablePath = $"bin/{JavaExecutable}";
+        public const string JavaExecutableExtension = "*";
+        public const string OsSymbol = "linux";
+    }
+
+    class Osx : Linux
+    {
+        public new const string JavaExecutablePath = $"Contents/Home/bin/{JavaExecutable}";
+        public new const string OsSymbol = "osx";
+    }
 }
