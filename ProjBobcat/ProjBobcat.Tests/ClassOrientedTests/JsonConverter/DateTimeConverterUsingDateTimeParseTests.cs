473a1309a41136e6e46d24890fe55a77dbeeb5b1
yueyinqiu <yueyinqiu@outlook.com>
2024-04-03 10:09:05 +0800

    perhaps enough test for datetime converter

--- ProjBobcat/ProjBobcat.Tests/ClassOrientedTests/JsonConverter/DateTimeConverterUsingDateTimeParseTests.cs
+++ ProjBobcat/ProjBobcat.Tests/ClassOrientedTests/JsonConverter/DateTimeConverterUsingDateTimeParseTests.cs
@@ -1,4 +1,5 @@
 ﻿using ProjBobcat.JsonConverter;
+using System.Globalization;
 using System.Text.Json;
 
 namespace ProjBobcat.Tests.ClassOrientedTests.JsonConverter;
@@ -11,24 +12,39 @@ public class DateTimeConverterUsingDateTimeParseTests
     {
         var converter = new DateTimeConverterUsingDateTimeParse();
         var dateTime = DateTime.Now;
-        var jsonData = JsonSerializer.SerializeToUtf8Bytes($"{dateTime}");
-        Utf8JsonReader reader = new Utf8JsonReader(jsonData);
-        Assert.IsTrue(reader.Read());
-        var result = converter.Read(ref reader, typeof(DateTime), JsonSerializerOptions.Default);
-        Assert.AreEqual(dateTime.Ticks, result.Ticks, TimeSpan.FromSeconds(1).Ticks);
-        Assert.IsFalse(reader.Read());
+
+        IEnumerable<(string, bool)> formats = [
+            ("o", true), ("r", true), ("s", false)
+        ];
+        foreach (var (format, toUtc) in formats)
+        {
+            foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                var modifiedTime = toUtc ? dateTime.ToUniversalTime() : dateTime;
+                var jsonData = JsonSerializer.SerializeToUtf8Bytes(
+                    modifiedTime.ToString(format, culture));
+
+                Utf8JsonReader reader = new Utf8JsonReader(jsonData);
+                Assert.IsTrue(reader.Read());
+                var result = converter.Read(ref reader, typeof(DateTime), JsonSerializerOptions.Default);
+                Assert.IsFalse(reader.Read());
+
+                Assert.AreEqual(dateTime.Ticks, result.Ticks, TimeSpan.FromSeconds(1).Ticks);
+            }
+        }
     }
 
     [TestMethod()]
     public void WriteTest()
     {
         var converter = new DateTimeConverterUsingDateTimeParse();
-        using var stream = new MemoryStream();
         var dateTime = DateTime.Now;
+
+        using var stream = new MemoryStream();
         using (var writer = new Utf8JsonWriter(stream))
             converter.Write(writer, dateTime, JsonSerializerOptions.Default);
-
         var actual = stream.ToArray();
+
         var expected = JsonSerializer.SerializeToUtf8Bytes(dateTime);
         CollectionAssert.AreEqual(expected, actual);
     }
