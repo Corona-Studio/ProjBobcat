6f6b9828c2411f06fe28c0989bfbcfdda665ee91
laolarou <luolinrui726@outlook.com>
2025-02-23 12:01:42 -0800

    Update DownloadHelper.Multipart.cs

--- ProjBobcat/ProjBobcat/Class/Helper/Download/DownloadHelper.Multipart.cs
+++ ProjBobcat/ProjBobcat/Class/Helper/Download/DownloadHelper.Multipart.cs
@@ -18,7 +18,7 @@ namespace ProjBobcat.Class.Helper.Download;
 
 public static partial class DownloadHelper
 {
-    private const int DefaultPartialDownloadTimeoutMs = 500;
+    private const int DefaultPartialDownloadTimeoutMs = 3000;
 
     private static HttpClient Head => HttpClientHelper.HeadClient;
     private static HttpClient MultiPart => HttpClientHelper.MultiPartClient;
